7f7a85c74945817d5b686ecf95ed098ce4efa93a
Benny Lam <bennylam6@gmail.com>
2024-12-01 13:00:52 -0500

    Finish 2024 day 1 part 2

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,5 +1,7 @@
 # https://adventofcode.com/2024/day/1
 
+from collections import defaultdict
+
 
 def day1_file_read(filename):
     with open(filename, "r") as file:
@@ -22,12 +24,30 @@ def day1_part1(filename):
     list1.sort()
     list2.sort()
 
-    result = 0
+    results = 0
     for i in range(len(list1)):
-        result += abs(list1[i] - list2[i])
+        results += abs(list1[i] - list2[i])
 
-    print(f"Day 1 part 1 {filename} results: {result}")
+    print(f"Day 1 part 1 {filename} results: {results}")
+
+
+def day1_part2(filename):
+    list1, list2 = day1_file_read(filename)
+
+    # technically can just use Counter, but want to show work
+    occurrences = defaultdict(int)
+    for el in list1:
+        occurrences[el] += 1
+
+    results = 0
+    for el in list2:
+        results += el * occurrences[el]
+
+    print(f"Day 1 part 2 {filename} results: {results}")
 
 
 day1_part1("sample.txt")
 day1_part1("input.txt")
+
+day1_part2("sample.txt")
+day1_part2("input.txt")
