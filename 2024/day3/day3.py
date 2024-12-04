e7e568d89b12737e899bfeffedd4a4e2657d33a7
Benny Lam <bennylam6@gmail.com>
2024-12-03 22:00:18 -0500

    Finish 2024 day 3 part 2

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -33,5 +33,33 @@ def day3_part_1(filename):
     print(f"Day 3 part 1 {filename} results: {results}")
 
 
+def day3_part_2(filename):
+    data = day3_file_read(filename)
+
+    mul_reg = r"mul\(\d+,\d+\)"
+    do_reg = r"do\(\)"
+    dont_reg = r"don't\(\)"
+
+    # combine regex
+    match_reg = "|".join([mul_reg, do_reg, dont_reg])
+    tokens = re.findall(match_reg, data)
+
+    enabled = True
+    results = 0
+    for token in tokens:
+        # not Python 3.10, currently using 3.7.7 no switch statement
+        if re.match(do_reg, token):
+            enabled = True
+        elif re.match(dont_reg, token):
+            enabled = False
+        elif enabled:
+            results += calculate(token)
+
+    print(f"Day 3 part 2 {filename} results: {results}")
+
+
 day3_part_1("sample.txt")
 day3_part_1("input.txt")
+
+day3_part_2("sample2.txt")
+day3_part_2("input.txt")
