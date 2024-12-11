9e4878bd5c8b50e88053e0ff1e8720d2c67146f1
Benny Lam <bennylam6@gmail.com>
2024-12-10 22:24:23 -0500

    Finish day 10 part 2

--- 2024/day10/day10.py
+++ 2024/day10/day10.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2024/day/10
 
+from collections import defaultdict
 import os
 
 WORKING_DIR = os.path.dirname(__file__)
@@ -75,5 +76,41 @@ def day10_part_1(filename):
     print(f"Day 10 part 1 {os.path.basename(filename)} results: {results}")
 
 
+def dfs2(graph, starting_point):
+    work_list = [starting_point]
+    bound_y = len(graph)
+    bound_x = len(graph[0])
+    trail_heads = 0
+
+    while work_list:
+        item = work_list.pop()
+
+        if graph[item[0]][item[1]] == 9:
+            trail_heads += 1
+        else:
+            for dir in SEARCH_DIR:
+                new_pos = add_tuples(item, dir)
+                if is_point_valid(bound_x, bound_y, new_pos) and graph[new_pos[0]][
+                    new_pos[1]
+                ] == (graph[item[0]][item[1]] + 1):
+                    work_list.append(new_pos)
+
+    return trail_heads
+
+
+def day10_part_2(filename):
+    graph = day10_file_read(filename)
+    starting_points = get_starting_points(graph)
+
+    results = 0
+    for starting_point in starting_points:
+        results += dfs2(graph, starting_point)
+
+    print(f"Day 10 part 2 {os.path.basename(filename)} results: {results}")
+
+
 day10_part_1(f"{WORKING_DIR}/sample.txt")
 day10_part_1(f"{WORKING_DIR}/input.txt")
+
+day10_part_2(f"{WORKING_DIR}/sample.txt")
+day10_part_2(f"{WORKING_DIR}/input.txt")
