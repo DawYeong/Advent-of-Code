50fcef2e953b1ccddf8d91dc61129a310eec7159
Benny Lam <bennylam6@gmail.com>
2024-12-12 23:43:47 -0500

    Finish day 11 part 2

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -8,38 +8,64 @@ def day11_file_read(filename):
     return data.split(" ")
 
 
-def blink_stone(stone, blinks):
-    blink = 0
-    stones = [stone]
-    while blink < blinks:
-        blink += 1
+def rule(stone):
+    if stone == "0":
+        return ["1"]
+    elif len(stone) % 2 == 0:
+        # split stone
+        return [str(stone[0 : len(stone) // 2]), str(int(stone[len(stone) // 2 :]))]
+    else:
+        return [str(int(stone) * 2024)]
 
-        new_stones = []
-        for s in stones:
-            if s == "0":
-                new_stones.append("1")
-            elif len(s) % 2 == 0:
-                # split stone
-                new_stones.append(str(s[0 : len(s) // 2]))
-                new_stones.append(str(int(s[len(s) // 2 :])))
-            else:
-                new_stones.append(str(int(s) * 2024))
 
-        stones = new_stones
+def calculate_num_stones(initial_stones, num_blinks):
+    cache = {}
 
-    return len(stones)
+    def blink_stone(stone, blinks_left):
+        # no blinks left => no stone change
+        if blinks_left == 0:
+            return 1
+
+        # found in cache => don't recompute
+        if (stone, blinks_left) in cache:
+            return cache[(stone, blinks_left)]
+
+        num_stones = 0
+        # traverse new stones
+        new_stones = rule(stone)
+        for s in new_stones:
+            num_stones += blink_stone(s, blinks_left - 1)
+
+        # save results in cache
+        cache[(stone, blinks_left)] = num_stones
+
+        return num_stones
+
+    results = 0
+    for stone in initial_stones:
+        results += blink_stone(stone, num_blinks)
+
+    return results
 
 
 def day11_part_1(filename):
     initial_stones = day11_file_read(filename)
 
-    results = 0
-
-    for stone in initial_stones:
-        results += blink_stone(stone, 25)
+    results = calculate_num_stones(initial_stones, 25)
 
     print(f"Day 11 part 1 {filename} results: {results}")
 
 
+def day11_part_2(filename):
+    initial_stones = day11_file_read(filename)
+
+    results = calculate_num_stones(initial_stones, 75)
+
+    print(f"Day 11 part 2 {filename} results: {results}")
+
+
 day11_part_1("sample.txt")
 day11_part_1("input.txt")
+
+day11_part_2("sample.txt")
+day11_part_2("input.txt")
