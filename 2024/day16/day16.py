7feb35d0b3231a486ff27f0290f53b68325678cb
Benny Lam <bennylam6@gmail.com>
2024-12-16 22:47:45 -0500

    Finish day 16 part 2

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2024/day/16
 
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import reduce
 from heapq import *
@@ -53,19 +54,22 @@ def add_tuples(tuples: List[Tuple[int, int]]) -> Tuple[int, int]:
 
 
 def traverse(graph: Graph) -> int:
-    queue = [(0, graph.start, 0)]
+    queue = [(0, graph.start, 0, None)]
     visited = {}
     min_score = math.inf
+    predecessors = defaultdict(set)
+    end_predecessors = set()
 
     while queue:
-        # print(queue)
-        curr_score, pos, curr_dir = heappop(queue)
+        curr_score, pos, curr_dir, prev = heappop(queue)
 
         # if we have already seen this path => don't compute unless it is less
         if (pos, curr_dir) in visited and visited[(pos, curr_dir)] < curr_score:
             continue
 
         visited[((pos, curr_dir))] = curr_score
+        if prev != None:
+            predecessors[(pos, curr_dir)].add(prev)
 
         for i in range(len(DIRECTIONS)):
             # don't traverse backwards
@@ -77,24 +81,43 @@ def traverse(graph: Graph) -> int:
             if new_pos in graph.positions:
                 if curr_dir % 4 != i % 4:
                     # turn, but don't move!
-                    heappush(queue, (curr_score + 1000, pos, i))
+                    heappush(queue, (curr_score + 1000, pos, i, (pos, curr_dir)))
                 else:
                     if new_pos == graph.end:
-                        min_score = min(min_score, curr_score + 1)
+                        if min_score > curr_score + 1:
+                            min_score = curr_score + 1
+                            end_predecessors.add((pos, curr_dir))
                         continue
 
-                    heappush(queue, (curr_score + 1, new_pos, i))
+                    heappush(queue, (curr_score + 1, new_pos, i, (pos, curr_dir)))
 
-    return min_score
+    seats = {graph.end}
+    visited_nodes = set()
+    pos_list = list(end_predecessors)
+
+    # traverse backwards from end to start to get the nodes on the "best" path
+    while pos_list:
+        pos = pos_list.pop()
+
+        if pos in visited_nodes:
+            continue
+
+        visited_nodes.add(pos)
+        seats.add(pos[0])
+
+        pos_list.extend(list(predecessors[pos]))
+
+    return min_score, len(seats)
 
 
-def day16_part_1(filename: str):
+def day16(filename: str):
     graph = day16_file_read(filename)
-    results = traverse(graph)
+    min_score, tiles = traverse(graph)
 
-    print(f"Day 16 part 1 {filename} results: {results}")
+    print(f"Day 16 part 1 {filename} results: {min_score}")
+    print(f"Day 16 part 2 {filename} results: {tiles}")
 
 
-day16_part_1("sample1.txt")
-day16_part_1("sample2.txt")
-day16_part_1("input.txt")
+day16("sample1.txt")
+day16("sample2.txt")
+day16("input.txt")
