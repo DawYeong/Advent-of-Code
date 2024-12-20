1e8cf13265f008c5c55451be5a2b0822c639d188
Benny Lam <bennylam6@gmail.com>
2024-12-19 20:50:35 -0500

    Finish day 17 part 2

--- 2024/day17/day17.py
+++ 2024/day17/day17.py
@@ -1,6 +1,8 @@
 # https://adventofcode.com/2024/day/17
 
+import copy
 from dataclasses import dataclass
+import math
 import os
 import re
 
@@ -31,7 +33,7 @@ def day17_file_read(filename: str) -> Device:
     return Device(registers=[lines[0][0], lines[1][0], lines[2][0]], program=lines[3])
 
 
-def execute_program(device: Device):
+def execute_program(device: Device) -> list[int]:
     program_counter = 0
 
     out = []
@@ -72,15 +74,61 @@ def execute_program(device: Device):
 
         program_counter += 2
 
-    return ",".join(out)
+    return out
 
 
 def day17_part_1(filename: str):
     device = day17_file_read(filename)
     results = execute_program(device)
 
-    print(f'Day 17 part 1 {filename} results: {results}')
+    print(f"Day 17 part 1 {filename} results: {",".join(results)}")
+
+
+def octal_to_decimal(octal: str) -> int:
+    result = 0
+    for i in range(len(octal)):
+        result += int(octal[len(octal) - 1 - i]) * pow(8, i)
+
+    return result
+
+
+def day17_part_2(filename: str):
+    device = day17_file_read(filename)
+
+    # TESTING
+    # for i in range(8):
+    #     for j in range(8):
+    #         for k in range(8):
+    #             for m in range(8):
+    #                 print(f'Start: {i}, {j}, {k}, {m}')
+    #                 # device1 = copy.deepcopy(device)
+    #                 device.registers[0] = octal_to_decimal(str(i) + str(j) + str(k) + str(m))
+    #                 print(execute_program(device))
+
+    results = math.inf
+
+    def backtracking(curr_level: int, curr_value: int):
+        nonlocal results
+
+        for i in range(8):
+            value = curr_value + i
+            device_copy = copy.deepcopy(device)
+            device_copy.registers[0] = value
+            curr_result = execute_program(device_copy)
+            if int(curr_result[0]) == device.program[-curr_level]:
+                if curr_level == len(device.program):
+                    results = min(results, value)
+                else:
+                    backtracking(curr_level + 1, value * 8)
+
+    backtracking(1, 0)
+
+    print(f"Day 17 part 2 {filename} results: {results}")
 
 
 day17_part_1("sample.txt")
+day17_part_1("sample2.txt")
 day17_part_1("input.txt")
+
+day17_part_2("sample2.txt")
+day17_part_2("input.txt")
