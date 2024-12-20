bbdf14f0832d3d232a537875102d27fa6effb970
Benny Lam <bennylam6@gmail.com>
2024-12-20 00:03:55 -0500

    Finish day 18 part 2

--- 2024/day18/day18.py
+++ 2024/day18/day18.py
@@ -3,6 +3,7 @@
 from functools import reduce
 from operator import add
 import os
+import time
 
 if os.path.dirname(__file__):
     os.chdir(os.path.dirname(__file__))
@@ -27,7 +28,9 @@ def is_pos_in_bounds(grid_size: int, pos: tuple[int, int]) -> bool:
     return 0 <= pos[0] <= grid_size and 0 <= pos[1] <= grid_size
 
 
-def traverse(bytes: list[tuple[int, int]], grid_size: int, simulation_duration: int):
+def traverse(
+    bytes: list[tuple[int, int]], grid_size: int, simulation_duration: int
+) -> int:
     simulated_bytes = set(bytes[:simulation_duration])
     work_list = [((0, 0), 0)]
     visited = set()
@@ -60,5 +63,33 @@ def day18_part_1(filename: str, grid_size: int = 70, simulation_duration: int = 
     print(f"Day 18 part 1 {filename} results: {results}")
 
 
+# brute force...
+def find_first_blocked_byte(
+    bytes: list[tuple[int, int]], grid_size: int
+) -> tuple[int, int] | None:
+    for i in range(len(bytes)):
+        # progress logging
+        # if i % 50 == 0:
+        #     print(f"{i}/{len(bytes)}")
+
+        if (min_steps := traverse(bytes, grid_size, i + 1)) == -1:
+            return bytes[i]
+
+    return None
+
+
+def day18_part_2(filename: str, grid_size: int = 70):
+    start = time.time()
+    bytes = day18_file_read(filename)
+    first_blocked_byte = [str(el) for el in find_first_blocked_byte(bytes, grid_size)]
+
+    print(
+        f"Day 18 part 2 {filename} results: {",".join(first_blocked_byte)}, {time.time() - start}s"
+    )
+
+
 day18_part_1("sample.txt", 6, 12)
 day18_part_1("input.txt")
+
+day18_part_2("sample.txt", 6)
+day18_part_2("input.txt")
