3ffb1640a932005da1d059fb3baea4dcc2f32988
Benny Lam <bennylam6@gmail.com>
2024-12-21 02:34:43 -0500

    Finish day 19 part 2

--- 2024/day19/day19.py
+++ 2024/day19/day19.py
@@ -15,33 +15,37 @@ def day19_file_read(filename: str) -> tuple[list[str], list[str]]:
     return sections[0].split(", "), sections[1].split("\n")
 
 
-def is_towel_valid(towels: list[str], target_towel: str):
-    work_list = [""]
-
-    while work_list:
-        curr_item = work_list.pop()
-
-        if curr_item == target_towel:
-            return True
-
-        left = target_towel[len(curr_item) :]
-        for towel in towels:
-            if towel == left[: len(towel)]:
-                work_list.append(curr_item + towel)
-
-    return False
-
-
-def day19_part_1(filename: str):
+def day19(filename: str):
     towels, target_towels = day19_file_read(filename)
-
     valid_towels = 0
+    arrangements = 0
+    cache = dict()
+
+    def towel_arrangements(towels: list[str], curr_towel: str) -> int:
+        if not curr_towel:
+            return 1
+
+        if curr_towel in cache:
+            return cache[curr_towel]
+
+        count = 0
+        for towel in towels:
+            if curr_towel.startswith(towel):
+                count += towel_arrangements(towels, curr_towel[len(towel) :])
+
+        cache[curr_towel] = count
+
+        return count
+
     for target_towel in target_towels:
-        if is_towel_valid(towels, target_towel):
+        if count := towel_arrangements(towels, target_towel):
             valid_towels += 1
+            arrangements += count
 
     print(f"Day 19 part 1 {filename} results: {valid_towels}")
+    print(f"Day 19 part 2 {filename} results: {arrangements}")
 
 
-day19_part_1("sample.txt")
-day19_part_1("input.txt")
+day19("sample.txt")
+day19("test.txt")
+day19("input.txt")
