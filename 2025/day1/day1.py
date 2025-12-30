c4a2c6e4e9a795b3675b5e4a15a6cdf28885ceff
Benny Lam <bennylam6@gmail.com>
2025-12-30 14:46:11 -0500

    Finish day 1 part 2

--- 2025/day1/day1.py
+++ 2025/day1/day1.py
@@ -33,5 +33,36 @@ def day1_part1(filename):
         
 
 
+def day1_part2(filename):
+    curr_point = 50
+    rotations = day1_file_read(filename)
+    pwd = 0
+
+    for rotation in rotations:
+        direction = rotation[0]
+        distance = int(rotation[1:])
+        temp_point = curr_point
+        if direction == "L":
+            curr_point -= distance
+        else:
+            curr_point += distance
+
+        if curr_point == 0: 
+            pwd += 1
+        else:
+            cycles = abs(int(curr_point / 100))
+            
+            if curr_point < 0:
+                cycles += 1 if temp_point != 0 else 0
+            
+            pwd += cycles
+
+        curr_point %= 100
+
+    print(f"Day 1 part 2 {filename} results: {pwd}")
+
 day1_part1('sample.txt')
 day1_part1('input.txt')
+
+day1_part2('sample.txt')
+day1_part2('input.txt')
